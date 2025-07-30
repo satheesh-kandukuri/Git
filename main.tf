ae3843d3caaab4b73d92a843fc6836d20e0d312f
satheesh-kandukuri <satheeshkandukuri335@gmail.com>
2025-07-30 13:22:41 +0530

    added main.tf file

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "test-vm-01" {
+  ami                  = "ami-054b7fc3c333ac6d2"
+  instance_type        = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.test_profile.name.
+
+  tags = {
+    Name = "test-vm-01"
+  }
+}
