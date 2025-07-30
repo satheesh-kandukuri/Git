0c7e3e43fb522120c44e59fd8710c17f8addba3d
satheesh-kandukuri <satheeshkandukuri335@gmail.com>
2025-07-30 13:29:44 +0530

    modified instance type from t2.micro to t2.large

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test-vm-01" {
   ami                  = "ami-054b7fc3c333ac6d2"
-  instance_type        = "t2.micro"
+  instance_type        = "t2.large"
   iam_instance_profile = aws_iam_instance_profile.test_profile.name
 
   tags = {
