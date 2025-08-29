374eafaea0481242255efa64d2f69ee74c1828bf
satheesh-kandukuri <satheeshkandukuri335@gmail.com>
2025-08-29 20:38:29 +0530

    Terraform EC2 Instance Creation files..

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
 resource "aws_instance" "test-vm-01" {
-  ami                  = "ami-054b7fc3c333ac6d2"
-  instance_type        = "t2.large"
-  iam_instance_profile = aws_iam_instance_profile.test_profile.name
-
-  tags = {
-    Name = "test-vm-01"
-  }
+  ami                  = var.ami_id
+  instance_type        = var.instance_type
+  count                = 1
+  availability_zone    = var.availability_zone[1]
 }
-Interested =  ['AWS','Python','Terraform','Jenkins']
 
-GPT = ['Co-Pilot','Claude','Grok']
